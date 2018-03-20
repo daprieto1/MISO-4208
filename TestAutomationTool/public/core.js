960029058e4031162a8b97e35d2c36590ba2597b
daprieto1 <da.prieto1@uniandes.edu.co>
2018-03-19 21:48:36 -0500

    connect back test suite service

--- TestAutomationTool/public/core.js
+++ TestAutomationTool/public/core.js
@@ -1,41 +1,10 @@
 // public/core.js
 angular.module('automationTestingTool', ['ui.bootstrap'])
-    .controller('mainController', function ($scope) {
-        $scope.testSuites = [
-            {
-                name: 'Dolibarr login',
-                description: 'This is a very interesting test suite ovet Dolibarr login',
-                provider: 'cypress',
-                lastExecution: {
-                    date: (new Date()).toLocaleDateString('en-US', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric' }),
-                    state: 'success'
-                }
-            }, {
-                name: 'Dolibarr login',
-                description: 'This is a very interesting test suite ovet Dolibarr login',
-                provider: 'nigthwatch',
-                lastExecution: {
-                    date: (new Date()).toLocaleDateString('en-US', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric' }),
-                    state: 'success'
-                }
-            },
-            {
-                name: 'Dolibarr login',
-                description: 'This is a very interesting test suite ovet Dolibarr login',
-                provider: 'cucumber',
-                lastExecution: {
-                    date: (new Date()).toLocaleDateString('en-US', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric' }),
-                    state: 'success'
-                }
-            },
-            {
-                name: 'Dolibarr login',
-                description: 'This is a very interesting test suite ovet Dolibarr login',
-                provider: 'calabash',
-                lastExecution: {
-                    date: (new Date()).toLocaleDateString('en-US', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric' }),
-                    state: 'success'
-                }
-            }
-        ];
+    .controller('mainController', function ($scope, $http) {
+        $http.get('/api/testsuite')
+            .then(response => {
+                $scope.testSuites = response.data;
+                console.log($scope.testSuites);
+            }, err => console.log(err));
+
     });
